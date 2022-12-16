c572a97ab5c4fc46df1a9b2774f80020d7edb631
LassiLankinen <lassi22002@student.hamk.fi>
2022-12-16 16:05:26 +0200

    eka committi tehdään luokka Dishwasher

--- src/GitTehtava.java
+++ src/GitTehtava.java
@@ -2,8 +2,45 @@
 public class GitTehtava {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
+}//
+
+class Dishwasher
+{
+	private int programme;
+	private boolean isOn;
+	
+	
+	public boolean getIsOn()
+	{
+		return isOn;
+	}
+	
+	// default constructor
+	public Dishwasher()
+	{
+	
+	programme = 0;
+	isOn=false;
+	
+}
+
+
+	public Dishwasher(int chosenone)
+	{
+		isOn = false;
+		programme = chosenone;
+		
+		
+	}
+	
+	public Dishwasher(int chosenone, boolean turnOnOff)
+	{
+		programme = chosenone;
+		isOn = turnOnOff;
+	
+	}
 }
